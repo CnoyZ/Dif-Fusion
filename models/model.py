4cff0b8117e803e4a8c4e21974534f9c9ffe470a
CnoyZ <117410853+CnoyZ@users.noreply.github.com>
2024-12-09 14:29:57 +0000

    Update model.py

--- models/model.py
+++ models/model.py
@@ -162,7 +162,7 @@ class DDPM(BaseModel):
         if load_path is not None:
             logger.info(
                 'Loading pretrained model for G [{:s}] ...'.format(load_path))
-            gen_path = '{}_gen.pth'.format(load_path)
+            gen_path = '{}.pth'.format(load_path)
             # opt_path = '{}_opt.pth'.format(load_path)
             # gen
             network = self.netG
